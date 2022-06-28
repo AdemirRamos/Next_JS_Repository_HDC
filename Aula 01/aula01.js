f8a6ead04b4068e5f673e319212cba01ec3948d6
AdemirRamos <ademirramos634@gmail.com>
2022-06-27 22:48:48 -0300

    Adição da Aula 01
    Neste 'commit' foi adicionado o arquivo 'aula01' com anotações básicas sobre Next JS.

---
+++ Aula 01/aula01.js
@@ -0,0 +1,31 @@
+/*
+
+    O Que é Next JS?
+
+    * Next JS é um "framework" de React;
+
+    * Segundo a sua própria documentação: "deixa o nosso projeto pronto para a produção";
+
+    * Isso se dá pelo "Server-side Rendering", onde temos ganhos de performance e também melhorias para o SEO;
+
+    * Praticamente não precisamos configurar nada e temos, também, diversas ferramentas para nos apoiar no desenvolvimento.
+
+    Pré-requesitos:
+
+    * Conhecimento dos fundamentos de Javascript e React;
+
+    * Como o Next é um "framework" de React, precisamos saber pelo menos o básico de React;
+
+    * Como a linguagem principal é Javascript, vamos escrever, basicamente, em JS e utilizar recursos modernos (ES6+);
+
+    * Conhecimentos em HTML e CSS também serão aproveitados.
+
+    O que precisamos?
+
+    * Para rodar e instalar aplicativos de Next JS, vamos precisar ter o Node JS (comandos de npm/npx);
+
+    * Um editor de código;
+
+    * Isso é suficiente, o resto ficará a cargo do Next JS.
+
+*/
