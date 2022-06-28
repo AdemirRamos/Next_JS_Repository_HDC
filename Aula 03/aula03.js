041a4c1c6efe2f8c5ab808315b0d03eed3154b76
AdemirRamos <ademirramos634@gmail.com>
2022-06-27 23:50:07 -0300

    Adição da Aula 03
    Neste 'commit' foi adicionado o arquivo 'aula03.js' - onde estão contidas as anotações desta aula.

---
+++ Aula 03/aula03.js
@@ -0,0 +1,9 @@
+/*
+
+    Estrutura de Pastas
+
+    * Para utilizar bem algo, é fundamental conhecer como foi construído - e isso não é diferente com o Next JS;
+
+    * Com a criação do projeto, há também a criação de diversas pastas, arquivos e "scripts".
+
+*/
