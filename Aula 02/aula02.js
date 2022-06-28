a0550b2cf528b9740e8bdc8a19de04f6f50cc833
AdemirRamos <ademirramos634@gmail.com>
2022-06-27 23:38:30 -0300

    Adição da Aula 02
    Neste 'commit' foi adicionado o diretório 'Aula 02' com os seus dois arquivos: primeiro, arquivo 'aula02.js' com anotações sobre o assunto discutido na aula; segundo, 'index.js' com o código escrito nessa aula.

---
+++ Aula 02/aula02.js
@@ -0,0 +1,15 @@
+/*
+
+    "Hello, world!"
+
+    * Para criar um projeto com Next JS precisamos utilizar o comando (sem aspas) "npx create-next-app nome do projeto";
+
+    * Um script será executado e teremos a possibilidade de rodar o projeto;
+
+    * Para iniciá-lo, utilizamos (sem aspas) "npm run dev"
+
+    * A partir de agora as alterações de código já se refletem no que é visto no navegador.
+
+    P. S.: O npx é um executador de pacotes (de npm).
+
+*/
---
+++ Aula 02/index.js
@@ -0,0 +1,15 @@
+//Aula 02 - "Hello, world" No Next JS
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+        <h1>Hello, world!</h1>
+
+    </div>
+  )
+}
