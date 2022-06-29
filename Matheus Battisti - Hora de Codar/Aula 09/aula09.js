9b20cb0fdf7c21575800e281abbe10dcbf44d1be
AdemirRamos <ademirramos634@gmail.com>
2022-06-29 17:36:39 -0300

    Adição da Aula 09
    Neste 'commit' foram adicionados os arquivos que compõem a aula 09 (CSS no Next JS).

---
+++ Matheus Battisti - Hora de Codar/Aula 09/aula09.js
@@ -0,0 +1,13 @@
+/*
+
+    CSS e Next JS
+
+    * Há duas formas simples de estilizar aplicações de Next JS;
+
+    * Estilos Globais: podem ser adicionados no arquivo "globals.css";
+
+    * CSS Modules: para componentes e páginas, vamos utilizar o CSS Modules;
+
+    * Basta criar um arquivo ".css" com o nome do componente; exemplo: "Home.module.css".
+
+*/
---
+++ Matheus Battisti - Hora de Codar/Aula 09/pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      
+      <h1 className={styles.title}>Hello, world - Next JS</h1>
+
+    </div>
+  )
+}
