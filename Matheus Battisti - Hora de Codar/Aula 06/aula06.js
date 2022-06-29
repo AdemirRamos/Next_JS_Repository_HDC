cbb6e327fc0fe443531d36ab38950e22ab89321d
AdemirRamos <ademirramos634@gmail.com>
2022-06-28 23:57:12 -0300

    Adição da Aula 06
    Neste 'commit' foram adicionados os arquivos referentes à aula 06 (navegação entre páginas).

---
+++ Matheus Battisti - Hora de Codar/Aula 06/aula06.js
@@ -0,0 +1,11 @@
+/*
+
+    Navegando Entre Páginas
+
+    * Para navegar entre as páginas do projeto, vamos utilizar o componente "Link";
+
+    * O "componente Link" é semelhante ao React Router, porém esse é do Next;
+
+    * Precisamos passar o "link" da página pela "prop href" do componente;
+
+*/
