cb89890f96c94da84ef2dc3cdfac5c170fcf376d
AdemirRamos <ademirramos634@gmail.com>
2022-06-28 23:36:58 -0300

    Adição da Aula 05
    Neste 'commit' foram adicionados os arquivos referentes à aula 05 ('Nested Routes').

--- Matheus Battisti - Hora de Codar/Aula 05/aula05.js
+++ Matheus Battisti - Hora de Codar/Aula 05/aula05.js
@@ -1,5 +1,19 @@
 /*
 
-    //
+    Nested Reoutes
+
+    * As "Nested Routes" são rotas compostas, onde podemos criar estruturas mais complexas, como: "/products/shirt"
+
+    * A ideia é criar uma pasta chamada "products" em pages e os arquivos das páginas dentro dessa pasta; o reteamento funciona da mesma forma;
+
+    * Gerando as páginas baseadas em pastas e arquivos;
+
+    * O arquivo "index.js" em uma pasta, vira a rota base da Nested.
+
+    * O arquivo "products.js" pode ser acessado ao se digitar "/products" na URL do servidor onde está rodando a aplicação Next;
+
+    * Para transformar esse arquivo em uma "Nested Route", é preciso colocá-lo dentro da pasta "products" e renomeá-lo para "index.js";
+
+    Para mais detalhes, ver a aula 05 do curso de Next.
 
 */
