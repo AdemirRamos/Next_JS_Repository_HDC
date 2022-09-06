bb0bd62c3874116bcae4d8d9444f8f58dae7704e
AdemirRamos <ademirramos634@gmail.com>
2022-09-06 17:29:42 -0300

    Adição da Aula 15

---
+++ Matheus Battisti - Hora de Codar/Aula 15/aula15.js
@@ -0,0 +1,11 @@
+/*
+
+	Página 404 Customizada
+	
+	* Para criar uma página 404 customizada é simples!;
+
+	* Precisamos criar um arquivo "404.js" em "pages";
+
+	* E, a partir dele, desenvolvemos os componentes e estilos.
+
+*/
